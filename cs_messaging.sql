3d6b44d4ba652b30c31250581fc9c9dc20790de7
yuanchiam <ychiam@netflix.com>
2017-05-16 16:50:09 -0700

    Update cs_messaging.sql

--- cs_messaging.sql
+++ cs_messaging.sql
@@ -5,22 +5,31 @@ select
     msg.message_id,
     msg.country_iso_code,
     msg.status_desc,
-    msg.fail_reason_short_desc
+    msg.fail_reason_short_desc,
+    msgd.message_name,
+    msgd.channel
 from dse.msg_send_f msg
 join etl.cs_message_id_entry ids
 on msg.account_id=ids.account_id
+join dse.msg_message_d msgd
+on msg.message_id=msgd.message_id
 where msg.send_utc_dateint>20170101
 
 -- temp table
 
 select 
-    message_guid,
-    send_utc_dateint,
-    account_id,
-    message_id,
-    country_iso_code,
-    status_desc,
-    fail_reason_short_desc
-from dse.msg_send_f
-where message_id=12853
-and send_utc_dateint>20170501
+    a.message_guid,
+    a.send_utc_dateint,
+    a.account_id,
+    a.message_id,
+    a.country_iso_code,
+    a.status_desc,
+    a.fail_reason_short_desc,
+    b.message_name,
+    b.channel
+from dse.msg_send_f a
+join dse.msg_message_d b
+on a.message_id=b.message_id
+where a.message_id=12853
+and a.send_utc_dateint>20170501
+limit 100;
