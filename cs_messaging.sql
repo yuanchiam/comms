033f1d4ec699a10a09c0bb984900ca342abdd6a5
yuanchiam <ychiam@netflix.com>
2017-05-16 16:34:11 -0700

    Update cs_messaging.sql

--- cs_messaging.sql
+++ cs_messaging.sql
@@ -9,4 +9,18 @@ select
 from dse.msg_send_f msg
 join etl.cs_message_id_entry ids
 on msg.account_id=ids.account_id
+where msg.send_utc_dateint>20170101
 
+-- temp table
+
+select 
+    message_guid,
+    send_utc_dateint,
+    account_id,
+    message_id,
+    country_iso_code,
+    status_desc,
+    fail_reason_short_desc
+from dse.msg_send_f
+where message_id=12853
+and send_utc_dateint>20170501
